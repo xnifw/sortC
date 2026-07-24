71ef6d5d09e0d090c8d8a9a909994b5b9475132a
ArkFactorial <74909540+alsrjs0725@users.noreply.github.com>
2026-07-24 19:34:26 +0900

    Add bubble sort example

---
+++ bubble_sort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[] = {5, 2, 9, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
