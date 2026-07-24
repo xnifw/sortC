7c264cf7a9896cea035645aca2ebaefed8f036bb
xnifw <xnifw@users.noreply.github.com>
2026-07-24 20:50:33 +0900

    Rename bubble sort variables

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-    int arr[] = {5, 2, 9, 1, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int numbers[] = {5, 2, 9, 1, 3};
+    int length = sizeof(numbers) / sizeof(numbers[0]);
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for (int pass = 0; pass < length - 1; pass++) {
+        for (int index = 0; index < length - pass - 1; index++) {
+            if (numbers[index] > numbers[index + 1]) {
+                int temporary = numbers[index];
+                numbers[index] = numbers[index + 1];
+                numbers[index + 1] = temporary;
             }
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    for (int index = 0; index < length; index++) {
+        printf("%d ", numbers[index]);
     }
     printf("\n");
 
     return 0;
 }
+
